d209835c00187e6fbfdc0509d758cc7d1424e1ba
enknaran202 <enknaran202@gmail.com>
2015-04-06 22:10:11 -0400

    Update Assignmenttwo.java

--- Assignmenttwo.java
+++ Assignmenttwo.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class assignmenttwo {
 	static double one,two,three,four,five,sum,av;
 		public static void main(String[] args) {
+			System.out.println("1%2 equals "+1%2);
+			System.out.println("1.0/2 equals "+1.0/2);
+			System.out.println("5+7/2 equals "+5+7/2);
+			System.out.println("\"Beat\" + '' + \"Army\" results in an error");
+			System.out.print("6+13/5-35%3 equals ");
+			System.out.println(+ 6 + 13 / 5 - 35 % 3);
+			System.out.println("3.5*(5/4) equals "+3.5*(5/4));
+			System.out.println("(3.5*5)/4 equals "+(3.5*5)/4);
+			
 			System.out.println("Type in five numbers using spaces as a delimiter");
 			Scanner s = new Scanner(System.in).useDelimiter("\\s|\n");
 			one=s.nextDouble();
@@ -18,4 +27,3 @@ public class assignmenttwo {
 		}
 
 }
-
