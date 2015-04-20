6ec0f13218989ecf2365ebc18f89e322ebe121ce
enknaran202 <enknaran202@gmail.com>
2015-04-20 16:04:43 -0400

    Update SixThree.java

--- SixThree.java
+++ SixThree.java
@@ -1,4 +1,3 @@
-//Create a Java GUI using JFrame, JPanel, JTextFields to use Flow Layout, Border Layout, and Grid Layout.
 /*
 This application must have 4 JTextFields
 Use Array to create JTextFields
@@ -9,15 +8,18 @@ Use Border Layout
 public class SixThree {
 
 	public static void main(String[] args) {
+		
 		SixThreeLayout border=new SixThreeLayout();
 		SixThreeLayout flow=new SixThreeLayout();
 		SixThreeLayout grid=new SixThreeLayout();
 		
-		border.Border();
-		flow.Flow();
-		grid.Grid();
+		border.border();
+		flow.flow();
+		grid.grid();
+			
+	
 		
-		
+
 	}
 
 }
