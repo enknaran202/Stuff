ebd24b2fcfc952e550c40ef54a08d20d6cd0dc7d
enknaran202 <enknaran202@gmail.com>
2015-04-20 16:05:00 -0400

    Update SixThreeLayout.java

--- SixThreeLayout.java
+++ SixThreeLayout.java
@@ -1,43 +1,56 @@
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.TextField;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+ 
+
 public class SixThreeLayout extends JFrame{
-	TextField [] tf=new TextField [4];{
+	TextField [] tf=new TextField [4]; {
 	tf[0]=new TextField("1",20);
 	tf[1]=new TextField("2",20);
 	tf[2]=new TextField("3",20);
-	tf[3]=new TextField("4",20);}
-	private JPanel Border;
-	private JPanel Flow;
-	private JPanel Grid;
+	tf[3]=new TextField("4",20);
+	}
+	private JPanel border=new JPanel();
+	private JPanel flow=new JPanel();
+	private JPanel grid=new JPanel();
+	
+	public void border(){
+		border.setLayout(new BorderLayout());
+		setVisible(true);
+		setSize(new Dimension(500,500));
+		border.add(tf[0],BorderLayout.PAGE_START);
+		border.add(tf[1],BorderLayout.CENTER);
+		border.add(tf[2],BorderLayout.PAGE_END);
+		border.add(tf[3],BorderLayout.LINE_END);
+		 
+		add(border);
+	}
+	public void flow() {
+		flow.setLayout(new FlowLayout());
+		setVisible(true);
+		setSize(new Dimension(500,500));
+		flow.add(tf[0]);
+		flow.add(tf[1]);
+		flow.add(tf[2]);
+		flow.add(tf[3]);
+		
+		add(flow);
 
-	public void Border() {
-		setLayout(new BorderLayout());
-		setVisible(true);
-		setSize(new Dimension(500,500));
-		add(tf[0],BorderLayout.PAGE_START);
-		add(tf[1],BorderLayout.CENTER);
-		add(tf[2],BorderLayout.PAGE_END);
-		add(tf[3],BorderLayout.LINE_END);
 	}
-	public void Flow() {
-		setLayout(new FlowLayout());
+	public void grid(){
+		grid.setLayout(new GridLayout(2,2));
 		setVisible(true);
 		setSize(new Dimension(500,500));
-		setLayout(new FlowLayout());
-		add(tf[0]);
-		add(tf[1]);
-		add(tf[2]);
-		add(tf[3]);
-	}
-	public void Grid() {
-		setLayout(new GridLayout());
-		setVisible(true);
-		setSize(new Dimension(500,500));
-		setLayout(new GridLayout(2,2));
-		add(tf[0]);
-		add(tf[1]);
-		add(tf[2]);
-		add(tf[3]);
+		grid.add(tf[0]);
+		grid.add(tf[1]);
+		grid.add(tf[2]);
+		grid.add(tf[3]);
+		
+		add(grid);
 	}
 }
